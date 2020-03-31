48acda6d0a63d07772369c47dbe5708e1d7c0f4c
Andry <andreymahdison@gmail.com>
2020-03-31 14:29:11 +0700

    Initial Commit

---
+++ src/test/java/com/summon23/springraxjava/SpringraxjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summon23.springraxjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringraxjavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
